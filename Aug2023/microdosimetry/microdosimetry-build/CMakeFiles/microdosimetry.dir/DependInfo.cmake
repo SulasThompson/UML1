71f2b29825cc55cb63763b4e972d88a70c36ac1b
SulasThompson <Dst@thompsonphysics.com>
2023-10-15 22:59:53 -0400

    added microdosimetry playspace, need target definition in detectorconstructor prior to import to alphaE

---
+++ Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry.cc" "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/microdosimetry.cc.o"
+  "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/src/ActionInitialization.cc" "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/src/ActionInitialization.cc.o"
+  "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/src/CommandLineParser.cc" "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/src/CommandLineParser.cc.o"
+  "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/src/DetectorConstruction.cc" "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/src/DetectorConstruction.cc.o"
+  "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/src/G4ElectronCapture.cc" "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/src/G4ElectronCapture.cc.o"
+  "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/src/PhysicsList.cc" "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/src/PhysicsList.cc.o"
+  "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/src/PrimaryGeneratorAction.cc" "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/src/RunAction.cc" "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/src/RunAction.cc.o"
+  "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/src/RunInitObserver.cc" "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/src/RunInitObserver.cc.o"
+  "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/src/SteppingAction.cc" "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/src/SteppingAction.cc.o"
+  "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/src/TrackingAction.cc" "/home/dthompson/Dropbox/UML1/Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/src/TrackingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_TOOLSSG_X11_GLES"
+  "PTL_BUILD_DLL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/dthompson/geant4-v11.1.1-install/include/Geant4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Aug2023/microdosimetry/microdosimetry-build/CMakeFiles/microdosimetry.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/microdosimetry.dir/microdosimetry.cc.o"
+  "CMakeFiles/microdosimetry.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/microdosimetry.dir/src/CommandLineParser.cc.o"
+  "CMakeFiles/microdosimetry.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/microdosimetry.dir/src/G4ElectronCapture.cc.o"
+  "CMakeFiles/microdosimetry.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/microdosimetry.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/microdosimetry.dir/src/RunAction.cc.o"
+  "CMakeFiles/microdosimetry.dir/src/RunInitObserver.cc.o"
+  "CMakeFiles/microdosimetry.dir/src/SteppingAction.cc.o"
+  "CMakeFiles/microdosimetry.dir/src/TrackingAction.cc.o"
+  "microdosimetry"
+  "microdosimetry.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/microdosimetry.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
